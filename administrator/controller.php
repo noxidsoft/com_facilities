3081cc078ce25f5651e45f62e9f0868e8a807591
Spudley <simon@simonchampion.net>
2015-11-04 10:51:51 +0000

    Update controller.php

--- administrator/controller.php
+++ administrator/controller.php
@@ -11,7 +11,7 @@
 // No direct access
 defined('_JEXEC') or die;
 
-class FacilitiesController extends JController
+class FacilitiesController extends JControllerLegacy
 {
 	/**
 	 * Method to display a view.
@@ -19,7 +19,7 @@ class FacilitiesController extends JController
 	 * @param	boolean			$cachable	If true, the view output will be cached
 	 * @param	array			$urlparams	An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 *
-	 * @return	JController		This object to support chaining.
+	 * @return	JControllerLegacy	This object to support chaining.
 	 * @since	1.5
 	 */
 	public function display($cachable = false, $urlparams = false)
